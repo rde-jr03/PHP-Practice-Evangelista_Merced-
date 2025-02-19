7d9f1ea9b6bfba501da27527e84d849cac8a54e5
LanceArvinMerced <202280126@psu.palawan.edu.ph>
2025-02-19 20:59:05 +0800

    Update array_of_fruits.php

--- Part 1/array_of_fruits.php
+++ Part 1/array_of_fruits.php
@@ -1,1 +1,22 @@
- 
+<?php
+
+$fruits = ["Apple", "Banana", "Orange", "Mango", "Grapes"];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Array of Fruits</title>
+</head>
+<body>
+    <h2>List of Fruits</h2>
+    <ol>
+        <?php
+       
+        for ($i = 0; $i < count($fruits); $i++) {
+            echo "<li>" . $fruits[$i] . "</li>";
+        }
+        ?>
+    </ol>
+</body>
+</html>
