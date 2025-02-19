86d83f60153c0d9681bb3750196fe4ffc5722d85
LanceArvinMerced <202280126@psu.palawan.edu.ph>
2025-02-19 20:59:54 +0800

    Update array_of_even_numbers.php

--- Part 1/array_of_even_numbers.php
+++ Part 1/array_of_even_numbers.php
@@ -1,1 +1,36 @@
- 
+ <?php
+
+$matrix = [
+    [12, 23, 34], 
+    [45, 55, 62], 
+    [71, 84, 90]
+];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Even Numbers from Matrix</title>
+</head>
+<body>
+    <h2>Even Numbers from the Matrix</h2>
+    <ul>
+        <?php
+     
+        $i = 0;
+        while ($i < count($matrix)) {
+            
+            $j = 0;
+            while ($j < count($matrix[$i])) {
+                if ($matrix[$i][$j] % 2 == 0) {
+                    echo "<li>" . $matrix[$i][$j] . "</li>";
+                }
+                $j++;
+            }
+            $i++;
+        }
+        ?>
+    </ul>
+</body>
+</html>
+
